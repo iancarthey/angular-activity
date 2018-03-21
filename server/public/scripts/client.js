3644934048b8055817c57ef815fdea2946899e1f
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-03-21 13:30:36 -0500

    completed stretch

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,9 @@
 console.log('wooHOO');
 
 let myApp = angular.module('myApp', []);
+
+myApp.controller('activityController', function(){
+    console.log('controlla is working');
+    let self = this;
+    self.greeting = 'my dream of being a wizard is finally real';
+})
